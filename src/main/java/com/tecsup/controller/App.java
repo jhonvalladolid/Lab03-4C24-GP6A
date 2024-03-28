ac183e4da6db41c1f0fab91adb7d471ae39db457
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-03-27 23:15:38 -0500

    Solucionado el archivo App.java

--- src/main/java/com/tecsup/controller/App.java
+++ src/main/java/com/tecsup/controller/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "Valladolid Castro Jhon" );
         System.out.println( "Rafael mallasca Escobar!" );
+        System.out.println( "Rafael" );
     }
 }
